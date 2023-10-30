818e382e6c9aee5bd5509990c51ebe7b7bd905a2
Isaiah-Ambi <ambiisaiah24@gmail.com>
2023-10-30 20:17:07 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,31 @@
+let a = 823 + 7482 + 54156 + 516 + 546 + 98 + 7491 + 3541 + 8423 + 1561
+a = a/10
+console.log(a)
+
+let b = 823 - a
+b = b**2 
+let c = 7482 - a 
+c = c**2
+let d = 54156 - a 
+d = d**2
+let e = 516 - a 
+e = e**2
+let f = 546 - a
+f = f**2
+let g = 98 - a
+g = g**2
+let h = 3541 - a
+h = h**2
+let i = 3541 - a
+i = i**2
+let j = 8423 - a
+j = j**2
+let k = 1561 - a
+k = k**2
+
+
+let s = b + c + d + e + f + g + h + i + j + k
+
+console.log(s)
+
+console.log(s/9)
