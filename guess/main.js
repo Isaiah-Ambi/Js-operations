764958bda4d7879c73b33537c25c5e9d8c1f2529
Isaiah-Ambi <ambiisaiah24@gmail.com>
2023-11-10 21:25:04 +0100

    update

--- guess/main.js
+++ guess/main.js
@@ -1,10 +1,11 @@
-let chances = 3
+let chances = 10
 let luckyNum = 44
 
 do {
-   guess = prompt('lucky')
+   guess = prompt('guess a number between 0 and 100')
    
    chances -= 1
+   alert(chances + ' chances left')
    if (isNumber(guess)){
         if (guess > luckyNum ){console.log('too high')}
         else if ( guess < luckyNum){console.log('too low')}
@@ -17,6 +18,7 @@ do {
 } while (chances != 0 & guess != luckyNum);
 
 console.log('You Lost!')
+alert("You're out of chances!")
 
 function isNumber(input) {
     return !isNaN(input) && isFinite(input);
